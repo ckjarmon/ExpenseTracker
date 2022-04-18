ca9075ee394433d897c7a117408f0c095d41ab92
kyeou <CHRISTIANJARMON630@GMAIL.COM>
2022-04-18 13:22:26 -0700

    ...

---
+++ app/src/main/java/com/kyeou/expensetracker/AddExpense.java
@@ -0,0 +1,16 @@
+package com.kyeou.expensetracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddExpense extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_expense);
+
+
+    }
+}
--- app/src/main/java/com/kyeou/expensetracker/MainActivity.java
+++ app/src/main/java/com/kyeou/expensetracker/MainActivity.java
@@ -27,14 +27,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+/*
         Button btn = (Button)findViewById(R.id.addExpense);
         btn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                 setContentView(R.layout.addexpense);
             }
-        });
+        }); */
     }
 
 
