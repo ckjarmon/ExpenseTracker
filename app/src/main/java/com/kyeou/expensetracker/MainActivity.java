32da4b0f8223ee6aefa770759e6b231fa333e0ea
MikeB1124 <56797038+MikeB1124@users.noreply.github.com>
2022-04-18 10:34:42 -0700

    michael add expense layout

--- app/src/main/java/com/kyeou/expensetracker/MainActivity.java
+++ app/src/main/java/com/kyeou/expensetracker/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 
@@ -26,6 +27,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = (Button)findViewById(R.id.addExpense);
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                setContentView(R.layout.addexpense);
+            }
+        });
     }
 
 
