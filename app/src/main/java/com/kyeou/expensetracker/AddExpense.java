e123bfbcc43dda95ee18a47d28999ec764d70f24
MikeB1124 <56797038+MikeB1124@users.noreply.github.com>
2022-04-19 11:42:15 -0700

    date picker made

--- app/src/main/java/com/kyeou/expensetracker/AddExpense.java
+++ app/src/main/java/com/kyeou/expensetracker/AddExpense.java
@@ -2,6 +2,7 @@ package com.kyeou.expensetracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 
@@ -9,15 +10,39 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Calendar;
 
-public class AddExpense extends AppCompatActivity {
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+public class AddExpense extends AppCompatActivity implements DatePickerDialog.OnDateSetListener{
+    private TextView datetext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_expense);
+        datetext = findViewById(R.id.date_text);
+
+        findViewById(R.id.show_dialog).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDatePickerDailog();
+            }
+        });
     }
 
+
+    private void showDatePickerDailog(){
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+            this,
+                this,
+                Calendar.getInstance().get(Calendar.YEAR),
+                Calendar.getInstance().get(Calendar.MONTH),
+                Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
+        );
+        datePickerDialog.show();
+    }
 
 
 //get data that user types and write it to a file
@@ -34,4 +59,9 @@ public class AddExpense extends AppCompatActivity {
         }
     }
 
+    @Override
+    public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
+        String date = month + "/" + dayOfMonth + "/" + year;
+        datetext.setText(date);
+    }
 }
