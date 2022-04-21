44e0ab3ca572fb664b4d04aba47d6ee50f495448
kyeou <CHRISTIANJARMON630@GMAIL.COM>
2022-04-20 18:50:27 -0700

    cant write or read files now, have to figure out

--- app/src/main/java/com/kyeou/expensetracker/WriteReadHandle.java
+++ app/src/main/java/com/kyeou/expensetracker/WriteReadHandle.java
@@ -1,29 +1,24 @@
 package com.kyeou.expensetracker;//package main.cpp.headers;
 
-import java.io.*;
-import java.nio.charset.StandardCharsets;
-
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 
 public class WriteReadHandle {
    
 
 
-   public void createFiles() throws IOException{
-      FileOutputStream fos = null;
+   public void createFiles() throws IOException {
 
-      try {
-         fos = new FileOutputStream("user.json");
-         fos.write("".getBytes());
+      File file = new File("user.json");
+   file.createNewFile();
+     /* try {
+         fos = new FileOutputStream(file);
+         //fos.write("".getBytes());
       } catch (FileNotFoundException e) {
-         e.printStackTrace();
-      } catch (IOException e) {
          e.printStackTrace();
       } finally {
          if (fos != null) {
@@ -33,14 +28,14 @@ public class WriteReadHandle {
                e.printStackTrace();
             }
          }
-      }
-
+      }*/
+      file = new File("transactions.json");
+      file.createNewFile();
+      /*
       try {
          fos = new FileOutputStream("transactions.json");
-         fos.write("".getBytes());
+         //fos.write("".getBytes());
       } catch (FileNotFoundException e) {
-         e.printStackTrace();
-      } catch (IOException e) {
          e.printStackTrace();
       } finally {
          if (fos != null) {
@@ -50,7 +45,7 @@ public class WriteReadHandle {
                e.printStackTrace();
             }
          }
-      }
+      }*/
    }//end function
 
 
@@ -62,20 +57,12 @@ public class WriteReadHandle {
       //File file = new File(path, filename);
       File file = new File(filename);
       file.createNewFile();
-      FileOutputStream stream = new FileOutputStream(file);
+      new FileOutputStream(file);
 
 
-
-     FileWriter out = new FileWriter(file);
-      
-      try {
-         //out.write(ttw);
-         stream.write(ttw.getBytes());
-      } finally {
-
-         if (out != null) {
-            out.close();
-         }
+      try (FileWriter out = new FileWriter(file)) {
+         out.write(ttw);
+         //stream.write(ttw.getBytes());
       }
 
    }
@@ -84,32 +71,32 @@ public class WriteReadHandle {
       File file = new File(filename);
       //file.createNewFile();
       FileReader in = new FileReader(file);
-      String ret = "";
+      StringBuilder ret = new StringBuilder();
 
       try {
 
          int content;
          while ((content = in.read()) != -1) {
             //System.out.print((char) content);
-            ret += (char)content;
+            ret.append((char) content);
          }
       } finally {
-         if (in != null) {
-            in.close();
-         }
+         in.close();
       }
-      return ret;
+      return ret.toString();
    }
 
 
 
   
-   public static void main(String args[]) throws IOException {
+   public static void main(String[] args) throws IOException {
       //new WriteReadHandle("transactionsJSON.json", "[{\"Name\": \"FirstName LastName\",  \"A_O_T\": 0,  \"Budgets\": [1,2,3],\"Income\": 0,\"Scores\": [],\"SumDebits\": 0 }]");
       WriteReadHandle test = new WriteReadHandle();
       System.out.println(test.ReadHandle("transactionsJSON.json"));
       System.out.println(new WriteReadHandle().ReadHandle("transactionsJSON.json"));
    }//end main 
+
+
 }
 
 /*
