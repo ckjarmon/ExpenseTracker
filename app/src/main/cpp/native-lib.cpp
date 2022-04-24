eb9ddcdf518810e9477e55682ca47771873a2cff
kyeou <CHRISTIANJARMON630@GMAIL.COM>
2022-04-23 22:35:50 -0700

    called the file handles for verifylogin

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -32,17 +32,17 @@ std::fstream file("wtf.txt");
 }*/
 
 // first function should take in a string from files after reading and pass it as a parameter to one of these functions
-extern "C" JNIEXPORT jstring JNICALL Java_com_kyeou_expensetracker_AddExpense_addTrans(JNIEnv *env, jobject, jstring name, int day, int month, int year, float amount, jstring TRANS_JSON_PARM)
-{
-    std::string ret = (new TRANS_HANDLE(env->GetStringUTFChars(name, nullptr), new Date(month, day, year), amount))->addTrans(env->GetStringUTFChars(TRANS_JSON_PARM, nullptr));
-    return env->NewStringUTF(ret.c_str());
-}
 
 /*
 extern "C" JNIEXPORT void JNICALL Java_com_kyeou_expensetracker_AddExpense_recordDebits(JNIEnv *env, jobject)
 {
     u->recordDebits();
 }*/
+extern "C" JNIEXPORT jstring JNICALL Java_com_kyeou_expensetracker_AddExpense_addTrans(JNIEnv *env, jobject, jstring name, int day, int month, int year, float amount, jstring TRANS_JSON_PARM)
+{
+    std::string ret = (new TRANS_HANDLE(env->GetStringUTFChars(name, nullptr), new Date(month, day, year), amount))->addTrans(env->GetStringUTFChars(TRANS_JSON_PARM, nullptr));
+    return env->NewStringUTF(ret.c_str());
+}
 
 extern "C" JNIEXPORT jstring JNICALL Java_com_kyeou_expensetracker_AddExpense_getUSERSJSON(JNIEnv *env, jobject)
 {
@@ -58,15 +58,14 @@ extern "C" JNIEXPORT jstring JNICALL Java_com_kyeou_expensetracker_SignupPage_ge
 {
     return env->NewStringUTF(u->USERDUMP().c_str());
 }
+extern "C" JNIEXPORT jstring JNICALL Java_com_kyeou_expensetracker_SignupPage_getTRANSJSON(JNIEnv *env, jobject)
+{
+    return env->NewStringUTF(u->TRANSDUMP().c_str());
+}
 
 extern "C" JNIEXPORT jstring JNICALL Java_com_kyeou_expensetracker_NewUserFunds_getUSERSJSON(JNIEnv *env, jobject)
 {
     return env->NewStringUTF(u->USERDUMP().c_str());
-}
-
-extern "C" JNIEXPORT jstring JNICALL Java_com_kyeou_expensetracker_SignupPage_getTRANSJSON(JNIEnv *env, jobject)
-{
-    return env->NewStringUTF(u->TRANSDUMP().c_str());
 }
 
 extern "C" JNIEXPORT jstring JNICALL Java_com_kyeou_expensetracker_MainActivity_getUSERINFO(JNIEnv *env, jobject, jstring stringCALL)
@@ -74,25 +73,28 @@ extern "C" JNIEXPORT jstring JNICALL Java_com_kyeou_expensetracker_MainActivity_
     return env->NewStringUTF(u->getUSER_FIELD(env->GetStringUTFChars(stringCALL, nullptr)).c_str());
 }
 
-extern "C" JNIEXPORT jstring JNICALL Java_com_kyeou_expensetracker_SignupPage_userSignUp(JNIEnv *env, jobject, jstring user_info_json, jstring trans_info_json)
+extern "C" JNIEXPORT jstring JNICALL Java_com_kyeou_expensetracker_VerifyLogin_userLogIn(JNIEnv *env, jobject, jstring user_info_json, jstring trans_info_json)
 {
     u = new USER_HANDLE(env->GetStringUTFChars(user_info_json, nullptr), env->GetStringUTFChars(trans_info_json, nullptr));
     return env->NewStringUTF(u->USERDUMP().c_str());
 }
 
-extern "C" JNIEXPORT void JNICALL Java_com_kyeou_expensetracker_NewUserFunds_addBudget(JNIEnv *env, jobject, float amount)
+extern "C" JNIEXPORT jstring JNICALL Java_com_kyeou_expensetracker_SignupPage_userSignUp(JNIEnv *env, jobject, jstring user_info_json, jstring trans_info_json)
 {
-    u->addBudget(amount);
+    u = new USER_HANDLE(env->GetStringUTFChars(user_info_json, nullptr), env->GetStringUTFChars(trans_info_json, nullptr));
+    return env->NewStringUTF(u->USERDUMP().c_str());
 }
-
 extern "C" JNIEXPORT void JNICALL Java_com_kyeou_expensetracker_SignupPage_addBudget(JNIEnv *env, jobject, float amount)
 {
     u->addBudget(amount);
 }
-
 extern "C" JNIEXPORT void JNICALL Java_com_kyeou_expensetracker_SignupPage_setName(JNIEnv *env, jobject, jstring name)
 {
     u->setName(env->GetStringUTFChars(name, nullptr));
+}
+extern "C" JNIEXPORT void JNICALL Java_com_kyeou_expensetracker_NewUserFunds_addBudget(JNIEnv *env, jobject, float amount)
+{
+    u->addBudget(amount);
 }
 
 extern "C" JNIEXPORT void JNICALL Java_com_kyeou_expensetracker_PersonalInformation_setName(JNIEnv *env, jobject, jstring name)
