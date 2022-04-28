26657c495f4d6551ed49d5fe81590a83ba4538b8
Abraham Mendoza <abraham.mendoza@live.com>
2022-04-28 13:21:34 -0700

    Starting to addGoogle Oauth functionality
    We can now begin to sign in with a google account, but need to fix so it goes into main_activity as it currently stays at the login page

--- app/src/main/java/com/kyeou/expensetracker/loginPage.java
+++ app/src/main/java/com/kyeou/expensetracker/loginPage.java
@@ -11,7 +11,30 @@ import android.widget.EditText;
 import java.io.IOException;
 import java.io.OutputStream;
 
-public class loginPage extends AppCompatActivity {
+
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInStatusCodes;
+import com.google.android.gms.common.Scopes;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+public class loginPage extends AppCompatActivity implements
+        View.OnClickListener {
+
+    private static final int RC_SIGN_IN = 9001;
+    private GoogleSignInClient mGoogleSignInClient;
 
     static {
         System.loadLibrary("expensetracker");
@@ -25,6 +48,52 @@ public class loginPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestScopes(new Scope(Scopes.DRIVE_APPFOLDER))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        findViewById(R.id.googleButton).setOnClickListener(this);
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.googleButton:
+                signIn();
+                break;
+            case R.id.signOutButton:
+                //signOut();
+                Intent intent = new Intent(this, loginPage.class);
+                startActivity(intent);
+                break;
+            /*case R.id.disconnect_button:
+                revokeAccess();
+                break;*/
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        if (account != null && GoogleSignIn.hasPermissions(account, new Scope(Scopes.DRIVE_APPFOLDER))) {
+            //updateUI(account);
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        } else {
+            /*updateUI(null);*/
+
+        }
     }
 
     public void logIn(View view) throws IOException {
