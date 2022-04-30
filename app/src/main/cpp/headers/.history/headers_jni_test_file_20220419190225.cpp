42eb19b5c10cb56e739ce8c95cf11a5af69cf60e
kyeou <CHRISTIANJARMON630@GMAIL.COM>
2022-04-29 20:51:57 -0700

    gitignore working

--- app/src/main/cpp/headers/.history/headers_jni_test_file_20220419190225.cpp
+++
@@ -1,7 +0,0 @@
-#include 
-
-
-JNIEXPORT void JNICALL Java_headers_1jni_1test_1file_TESTSEQ
-  (JNIEnv *, jobject) {
-
-  }
--- app/src/main/cpp/headers/.history/headers_jni_test_file_20220419190227.cpp
+++
@@ -1,7 +0,0 @@
-#include "headers_jni_test_file copy.h"
-
-
-JNIEXPORT void JNICALL Java_headers_1jni_1test_1file_TESTSEQ
-  (JNIEnv *, jobject) {
-
-  }
