5104ccf84dabcf56975abcf0b94e997f458209e4
kyeou <CHRISTIANJARMON630@GMAIL.COM>
2022-04-29 21:00:14 -0700

    nuked all the history folders

--- app/src/.history/main/cpp/headers/new_componenets_test_20220420171511.cpp
+++
@@ -1,6 +0,0 @@
-#include "components.hpp"
-
-int main() {
-    std::string ret = (new TRANS_HANDLE(env->GetStringUTFChars(name, nullptr), new Date(month, day, year), amount))->addTrans(env->GetStringUTFChars(TRANS_JSON_PARM, nullptr));
- 
-}
